3d44edaef392e1bd7f0aadf74fbb558c2249cb77
Mariana <marpotichkin@gmail.com>
2021-09-26 14:35:48 -0300

    Tarjeta bodega

---
+++ vinos/src/components/Bodega/Bodega.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Typography, Container, Button } from '@material-ui/core';
+import useStyles from './styles';
+import BodegaDatos from './Bodega1';
+import bodegas from "../datos/bodegas.json";
+
+
+const Bodega = () => {
+    const classes = useStyles();
+    
+    return (
+        <>
+            <Container>
+                <div className={classes.barra} />
+
+                <Typography className={classes.titulo} align="center" variant="h4" color="inherit">
+                    BODEGAS
+                </Typography>
+
+                {bodegas.map(( bodega) => 
+                    <BodegaDatos
+                        id={bodega.id}
+                        nombre={bodega.nombre}
+                        telefono={bodega.telefono}
+                    />
+                )}
+            </Container>
+        </>
+    )
+}
+
+
+export default Bodega
