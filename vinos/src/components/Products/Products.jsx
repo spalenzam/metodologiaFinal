8fcfede6fa9833644623e06530afefc09423fa63
santi <chanti.aguilar@gmail.com>
2021-09-27 18:41:44 -0300

    segundo commit product

--- vinos/src/components/Products/Products.jsx
+++ vinos/src/components/Products/Products.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
-import Product from './Product/Product';
+import Producto from './Producto/Producto';
+import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$6500', image},
-    {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$556500', image},
-]
-
-const Products = () => {
+const Productos = ({ productos, agregarACarrito }) => {
     
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}> 
-                {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} >
-                        <Product product={product}/>
+                {productos.map((producto) => (
+                    <Grid item key={producto.id} xs={12} sm={6} md={4} lg={3} >
+                        <Producto product={producto} agregarACarrito={agregarACarrito}/>
                     </Grid>
                 )
                 )}
@@ -38,4 +37,4 @@ const Products = () => {
 
 
 
-export default Products
+export default Productos
