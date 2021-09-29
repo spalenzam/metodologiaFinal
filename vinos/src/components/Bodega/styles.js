ca756b5dae0982f6938800bf3d87d8c0d9cdb375
Mariana <marpotichkin@gmail.com>
2021-09-29 20:19:25 -0300

    actualizacion datos bodegas

---
+++ vinos/src/components/Bodega/styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+  barra: theme.mixins.toolbar,
+  titulo: {
+    marginTop: '3%',
+  },
+  subtitulo1: {
+    marginTop: '3%',
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+  subtitulo: {
+    marginTop: '4.5%',
+    marginBottom: '2%',
+    textDecoration: 'none',
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+  subtitulo2: {
+    marginTop: '4.5%',
+    textDecoration: 'none',
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+  subtitulo3: {
+    textDecoration: 'none',
+    color: 'inherit',
+    textDecoration: 'none',
+  }
+}))
